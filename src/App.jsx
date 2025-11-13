41af0c59bfec5787c412a39d00e829471c61a61a
dukubkubpom <kasidech.nea@spumail.net>
2025-11-13 19:01:53 +0700

    re commit

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import "./App.css";
 import Login from "./pages/Login/Login";
 
 function App() {
-  const [token, setToken] = useState("x")
+  const [token, setToken] = useState("")
   const [role, setRole] = useState("")
 
   const [products, setProducts] = useState([]);
